d73a723b06a9934922b45204c61609401b09e5b7
PyRoS <cautivo.chacon@gmail.com>
2017-09-18 19:50:36 +0200

    integrate redux + apollo client

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ require('../styles/cookbook.scss');
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 
 import App from './components/app';
 import reducers from './reducers';
@@ -14,10 +13,20 @@ const client = new ApolloClient({
     networkInterface: createNetworkInterface({ uri: 'https://api.graph.cool/simple/v1/cj7hw17os0ulx0108whavfg1r' }),
 });
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const store = createStore(
+    combineReducers({
+        apollo: client.reducer(),
+    }),
+    {}, // initial state
+    compose(
+        applyMiddleware(client.middleware()),
+        // If you are using the devToolsExtension, you can add it here also
+        (typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined') ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
+    )
+);
 
 ReactDOM.render(
-    <ApolloProvider client={client} store={createStoreWithMiddleware(reducers)}>
+    <ApolloProvider client={client} store={store}>
         <App />
     </ApolloProvider>
     , document.querySelector('.container'));
