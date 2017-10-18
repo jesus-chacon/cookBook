65158458e2903ab6b15adfa0fd76f05397ee5bba
Jesús Cautivo Chacón <cautivo.chacon@gmail.com>
2017-10-18 19:34:34 +0200

    Add subscriptions

--- src/components/recipes/index.js
+++ src/components/recipes/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { graphql, gql, compose } from 'react-apollo';
+import { graphql, gql } from 'react-apollo';
 import { connect } from 'react-redux';
 
 import { ChangeBackground } from "../../actions/background";
@@ -8,7 +8,7 @@ import RecipeCard from './card';
 class RecipesIndex extends Component {
     renderRecipes(){
         return (
-            this.props.data.allRecipes.map(recipe => (
+            this.props.allRecipes.allRecipes.map(recipe => (
                 <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3 equalHeightCol" style={{paddingTop: '5px', paddingBottom: '15px'}} key={recipe.id}>
                     <RecipeCard recipe={recipe} updateCacheAfterLike={this._updateCacheAfterLike} updateCacheAfterRemoveLike={this._updateCacheAfterRemoveLike}/>
                 </div>
@@ -17,9 +17,9 @@ class RecipesIndex extends Component {
     }
 
     render(){
-        if (this.props.data.loading){
+        if (this.props.allRecipes.loading){
             return (<h1 className="text-info">Loading...</h1>);
-        } else if (this.props.data.error){
+        } else if (this.props.allRecipes.error){
             return (<h1 className="text-danger">Error</h1>);
         } else {
             return (
@@ -35,11 +35,13 @@ class RecipesIndex extends Component {
     }
 
     componentDidMount(){
-        this.props.changeBackground("");
+        //this.props.changeBackground("");
+        this._subscribeToNewRecipes();
+        this._subscribeToNewLikes();
     }
 
     _updateCacheAfterLike = (store, createLike, recipeId) => {
-        // 1 Load the store
+        /*// 1 Load the store
         const data = store.readQuery({ query: ALL_RECIPES });
         // 2 modify the store
         let recipeLiked = data.allRecipes.find(recipe => recipe.id === recipeId);
@@ -53,11 +55,11 @@ class RecipesIndex extends Component {
         });
 
         // 3 updating the store
-        store.writeQuery({ query: ALL_RECIPES, data });
+        store.writeQuery({ query: ALL_RECIPES, data });*/
     };
 
     _updateCacheAfterRemoveLike = (store, deleteLike, recipeId, likeId) => {
-        const data = store.readQuery({ query: ALL_RECIPES });
+        /*const data = store.readQuery({ query: ALL_RECIPES });
 
         data.allRecipes = _.map(data.allRecipes, recipe => {
             if (recipe.id === recipeId) {
@@ -67,7 +69,72 @@ class RecipesIndex extends Component {
             return recipe;
         });
 
-        store.writeQuery({ query: ALL_RECIPES, data });
+        store.writeQuery({ query: ALL_RECIPES, data });*/
+    };
+
+    _subscribeToNewRecipes = () => {
+        this.props.allRecipes.subscribeToMore({
+            document: gql`
+                subscription {
+                    Recipe(filter: { mutation_in: [CREATED] }) {
+                        node {
+                            id,
+                            title,
+                            imageUrl,
+                            summary,
+                            likes {
+                                id,
+                                user {
+                                    id
+                                }
+                            }
+                        }
+                    }
+                }
+            `,
+            updateQuery: (previous, {subscriptionData}) => {
+                const newAllRecipes = [subscriptionData.data.Recipe.node, ...previous.allRecipes];
+                return { ...previous, allRecipes: newAllRecipes };
+            }
+        });
+    };
+
+    _subscribeToNewLikes = () => {
+        this.props.allRecipes.subscribeToMore({
+            document: gql`
+                subscription {
+                    Like(filter: { mutation_in: [CREATED] }) {
+                        node {
+                            id, 
+                            recipe {
+                                id,
+                                title,
+                                imageUrl,
+                                summary,
+                                likes {
+                                    id,
+                                    user {
+                                        id
+                                    }
+                                }
+                            },
+                            user {
+                               id
+                            }
+                        }
+                    }
+                }
+            `,
+            updateQuery: (previous, {subscriptionData}) => {
+                console.log('update', subscriptionData);
+                const likeRecipeIndex = previous.allRecipes.findIndex(recipe => recipe.id === subscriptionData.data.Like.node.recipe.id);
+                const recipe = subscriptionData.data.Like.node.recipe;
+                const newAllRecipes = previous.allRecipes.slice();
+                newAllRecipes[likeRecipeIndex] = recipe;
+
+                return { ...previous, allRecipes: newAllRecipes };
+            }
+        });
     };
 }
 
@@ -92,8 +159,6 @@ export const ALL_RECIPES = gql`
     }
 `;
 
-const component = compose(
-    graphql(ALL_RECIPES)
-)(RecipesIndex);
-
-export default connect((state) => ({state}), mapDispatchToProps)(component);
+const component = graphql(ALL_RECIPES, {name: 'allRecipes'})(RecipesIndex);
+export default component;
+//export default connect((state) => ({state}), mapDispatchToProps)(component);
--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import promise from 'redux-promise';
 import { BrowserRouter } from 'react-router-dom';
-import ApolloClient, { createNetworkInterface, ApolloProvider } from 'react-apollo'
+import ApolloClient, { createNetworkInterface, ApolloProvider } from 'react-apollo';
+import { SubscriptionClient, addGraphQLSubscriptions } from 'subscriptions-transport-ws';
 
 import App from './components/app';
 
@@ -15,6 +16,13 @@ import sessionReducer from './reducers/session';
 import { GC_AUTH_TOKEN } from "./components/constants";
 
 const networkInterface = createNetworkInterface({ uri: 'https://api.graph.cool/simple/v1/cj7hw17os0ulx0108whavfg1r' });
+
+const wsClient = new SubscriptionClient('wss://subscriptions.graph.cool/v1/cj7hw17os0ulx0108whavfg1r',{
+    reconnect: true,
+    connectionParams: {
+        authToken: localStorage.getItem(GC_AUTH_TOKEN),
+    }
+});
 
 networkInterface.use([{
     applyMiddleware(req, next) {
@@ -27,8 +35,13 @@ networkInterface.use([{
     }
 }]);
 
-const client = new ApolloClient({
+const networkInterfaceWithSubscriptions = addGraphQLSubscriptions(
     networkInterface,
+    wsClient
+);
+
+const client = new ApolloClient({
+    networkInterface: networkInterfaceWithSubscriptions
 });
 
 const store = createStore(
